ba04441dda77cc2ea8eb4271ea787fb801533847
luisluis24 <luiselpro2424@gmail.com>
2025-06-02 12:21:12 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,12 @@
-const CACHE_NAME = 'gym-progress-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/style.css', // si tienes uno separado
-  '/service-worker.js',
-  '/icon-192.png',
-  '/icon-512.png'
-];
-
-// Instalar el Service Worker y guardar archivos en caché
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', e => {
+  console.log('Service Worker instalado');
+  self.skipWaiting();
 });
 
-// Activar el SW y limpiar versiones antiguas
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      )
-    )
-  );
+self.addEventListener('activate', e => {
+  console.log('Service Worker activado');
 });
 
-// Interceptar peticiones y servir desde caché si es posible
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
-  );
+self.addEventListener('fetch', e => {
+  console.log('Fetch interceptado:', e.request.url);
 });
